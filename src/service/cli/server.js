385fb9f591ceb8609c076ca2ba6e3df1466d7459
Ivan Savitsky <ivan@savitsky.biz>
2020-10-26 18:30:14 +0300

    Add logging

--- src/service/cli/server.js
+++ src/service/cli/server.js
@@ -1,28 +1,53 @@
 'use strict';
 
 const express = require(`express`);
-const chalk = require(`chalk`);
-
 const routes = require(`../api`);
 
-const {API_PREFIX, DEFAULT_PORT} = require(`../constants`);
+const {API_PREFIX, DEFAULT_PORT, HttpCode} = require(`../constants`);
+const {getLogger} = require(`../lib/logger`);
 
 const app = express();
+const logger = getLogger({name: `api`});
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
+app.use((req, res, next) => {
+  logger.debug(`Request on route ${req.url}`);
+  res.on(`finish`, () => {
+    logger.info(`Response status code ${res.statusCode}`);
+  });
+  next();
+});
+
 app.use(API_PREFIX, routes);
+
+app.use((req, res) => {
+  res.status(HttpCode.NOT_FOUND)
+    .send(`Not found`);
+  logger.error(`Route not found: ${req.url}`);
+});
+
+app.use((err, _req, _res, _next) => {
+  logger.error(`An error occurred on processing request: ${err.message}`);
+});
 
 module.exports = {
   name: `--server`,
   run(args) {
     const [customPort] = args;
     const port = Number.parseInt(customPort, 10) || DEFAULT_PORT;
-    if (port > 0 && port <= 65535) {
-      app.listen(DEFAULT_PORT, () => console.log(`Сервер запущен на порту: ${DEFAULT_PORT}`));
-    } else {
-      console.error(chalk.red(`Please enter port number between 1 and 65535`));
+
+    try {
+      app.listen(port, (err) => {
+        if (err) {
+          return logger.error(`An error occurred on server creation: ${err.message}`);
+        }
+        return logger.info(`Listening to connections on ${port}`);
+      });
+    } catch (err) {
+      logger.error(`An error occurred: ${err.message}`);
+      process.exit(1);
     }
   }
 };
