624422e277f4affc98d8c0220f48ba9a92b1b007
Ivan Savitsky <ivan@savitsky.biz>
2021-07-03 21:23:40 +0300

    Add sessions

--- src/express/index.js
+++ src/express/index.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const express = require(`express`);
+const session = require(`express-session`);
 const bodyParser = require(`body-parser`);
 const homeRoutes = require(`./routes/home`);
 const registerRoutes = require(`./routes/register`);
@@ -12,7 +13,24 @@ const categoriesRoutes = require(`./routes/categories`);
 const path = require(`path`);
 const {DEFAULT_PORT, PUBLIC_DIR, UPLOAD_DIR} = require(`./constants`);
 
+const sequelize = require(`../service/lib/sequelize`);
+const SequelizeStore = require(`connect-session-sequelize`)(session.Store);
+
+const {SESSION_SECRET} = process.env;
+if (!SESSION_SECRET) {
+  throw new Error(`SESSION_SECRET environment variable is not defined`);
+}
+
 const app = express();
+
+const mySessionStore = new SequelizeStore({
+  db: sequelize,
+  expiration: 180000,
+  checkExpirationInterval: 60000
+});
+
+sequelize.sync({force: false});
+
 app.use(express.static(path.resolve(__dirname, PUBLIC_DIR)));
 app.use(express.static(path.resolve(__dirname, UPLOAD_DIR)));
 
@@ -20,6 +38,14 @@ app.set(`views`, path.resolve(__dirname, `templates/pages`));
 app.set(`view engine`, `pug`);
 
 app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(session({
+  secret: SESSION_SECRET,
+  store: mySessionStore,
+  resave: false,
+  proxy: true,
+  saveUninitialized: false,
+}));
 
 app.use(`/`, homeRoutes);
 app.use(`/register`, registerRoutes);
